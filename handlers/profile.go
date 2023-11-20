c8254db2a255899d5af2457025ecd0bd203263a4
Pana77 <101635157+Pana77@users.noreply.github.com>
2023-11-20 01:05:31 +0100

    validations

--- handlers/profile.go
+++ handlers/profile.go
@@ -2,8 +2,10 @@ package handlers
 
 import (
 	"context"
+	"errors"
 	protos "github.com/MihajloJankovic/profile-service/protos/main"
 	"log"
+	"strings"
 )
 
 type myProfileServer struct {
@@ -19,6 +21,10 @@ func NewServer(l *log.Logger, r *ProfileRepo) *myProfileServer {
 
 // add edit,create user ,delete user
 func (s myProfileServer) GetProfile(ctx context.Context, in *protos.ProfileRequest) (*protos.ProfileResponse, error) {
+	// Validate required field
+	if in.GetEmail() == "" {
+		return nil, errors.New("Invalid input. Email is required.")
+	}
 
 	out, err := s.repo.GetById(in.GetEmail())
 	if err != nil {
@@ -27,7 +33,19 @@ func (s myProfileServer) GetProfile(ctx context.Context, in *protos.ProfileReque
 	}
 	return out, nil
 }
-func (s myProfileServer) SetProfile(kon context.Context, in *protos.ProfileResponse) (*protos.Empty, error) {
+
+func (s myProfileServer) SetProfile(ctx context.Context, in *protos.ProfileResponse) (*protos.Empty, error) {
+	// Validate required fields
+	if in.GetEmail() == "" || in.GetFirstname() == "" || in.GetLastname() == "" {
+		return nil, errors.New("Invalid input. Email, firstname, and lastname are required.")
+	}
+
+	// Validate email format
+	if !isValidEmailFormat(in.GetEmail()) {
+		return nil, errors.New("Invalid email format.")
+	}
+
+	// Additional validation for other fields if needed
 
 	out := new(protos.ProfileResponse)
 	out.Email = in.GetEmail()
@@ -44,10 +62,29 @@ func (s myProfileServer) SetProfile(kon context.Context, in *protos.ProfileRespo
 	}
 	return new(protos.Empty), nil
 }
-func (s myProfileServer) UpdateProfile(kon context.Context, in *protos.ProfileResponse) (*protos.Empty, error) {
+
+func (s myProfileServer) UpdateProfile(ctx context.Context, in *protos.ProfileResponse) (*protos.Empty, error) {
+	// Validate required fields
+	if in.GetEmail() == "" || in.GetFirstname() == "" || in.GetLastname() == "" {
+		return nil, errors.New("Invalid input. Email, firstname, and lastname are required.")
+	}
+
+	// Validate email format
+	if !isValidEmailFormat(in.GetEmail()) {
+		return nil, errors.New("Invalid email format.")
+	}
+
+	// Additional validation for other fields if needed
+
 	err := s.repo.Update(in)
 	if err != nil {
 		return nil, err
 	}
 	return new(protos.Empty), nil
+}
+
+// isValidEmailFormat checks if the given email is in a valid format.
+func isValidEmailFormat(email string) bool {
+	// Perform a simple check for '@' and '.com'
+	return strings.Contains(email, "@") && strings.HasSuffix(email, ".com")
 }
