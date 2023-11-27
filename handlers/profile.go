e26938dd95a8fbc4642f41e0520401c845482af3
Pana77 <101635157+Pana77@users.noreply.github.com>
2023-11-27 11:58:18 +0100

    trim validations added

--- handlers/profile.go
+++ handlers/profile.go
@@ -26,7 +26,7 @@ func (s myProfileServer) GetProfile(ctx context.Context, in *protos.ProfileReque
 		return nil, errors.New("Invalid input. Email is required.")
 	}
 
-	out, err := s.repo.GetById(in.GetEmail())
+	out, err := s.repo.GetById(strings.TrimSpace(in.GetEmail())) // Added trim here
 	s.logger.Println(out)
 	if err != nil {
 		s.logger.Println(err)
@@ -37,25 +37,25 @@ func (s myProfileServer) GetProfile(ctx context.Context, in *protos.ProfileReque
 
 func (s myProfileServer) SetProfile(ctx context.Context, in *protos.ProfileResponse) (*protos.Empty, error) {
 	// Validate required fields
-	if in.GetEmail() == "" || in.GetFirstname() == "" || in.GetLastname() == "" || in.GetUsername() == "" {
+	if strings.TrimSpace(in.GetEmail()) == "" || strings.TrimSpace(in.GetFirstname()) == "" || strings.TrimSpace(in.GetLastname()) == "" || strings.TrimSpace(in.GetUsername()) == "" {
 		return nil, errors.New("Invalid input. Email, firstname, username, and lastname are required.")
 	}
 
 	// Validate email format
-	if !isValidEmailFormat(in.GetEmail()) {
+	if !isValidEmailFormat(strings.TrimSpace(in.GetEmail())) { // Added trim here
 		return nil, errors.New("Invalid email format.")
 	}
 
 	// Additional validation for other fields if needed
 
 	out := new(protos.ProfileResponse)
-	out.Email = in.GetEmail()
-	out.Firstname = in.GetFirstname()
-	out.Lastname = in.GetLastname()
-	out.Birthday = in.GetBirthday()
-	out.Gender = in.GetGender()
-	out.Role = in.GetRole()
-	out.Username = in.Username
+	out.Email = strings.TrimSpace(in.GetEmail())         // Added trim here
+	out.Firstname = strings.TrimSpace(in.GetFirstname()) // Added trim here
+	out.Lastname = strings.TrimSpace(in.GetLastname())   // Added trim here
+	out.Birthday = strings.TrimSpace(in.GetBirthday())   // Added trim here
+	out.Gender = strings.TrimSpace(in.GetGender())       // Added trim here
+	out.Role = strings.TrimSpace(in.GetRole())           // Added trim here
+	out.Username = strings.TrimSpace(in.GetUsername())   // Added trim here
 
 	err := s.repo.Create(out)
 	if err != nil {
@@ -67,12 +67,12 @@ func (s myProfileServer) SetProfile(ctx context.Context, in *protos.ProfileRespo
 
 func (s myProfileServer) UpdateProfile(ctx context.Context, in *protos.ProfileResponse) (*protos.Empty, error) {
 	// Validate required fields
-	if in.GetEmail() == "" || in.GetFirstname() == "" || in.GetLastname() == "" {
+	if strings.TrimSpace(in.GetEmail()) == "" || strings.TrimSpace(in.GetFirstname()) == "" || strings.TrimSpace(in.GetLastname()) == "" {
 		return nil, errors.New("Invalid input. Email, firstname, and lastname are required.")
 	}
 
 	// Validate email format
-	if !isValidEmailFormat(in.GetEmail()) {
+	if !isValidEmailFormat(strings.TrimSpace(in.GetEmail())) { // Added trim here
 		return nil, errors.New("Invalid email format.")
 	}
 
@@ -85,16 +85,17 @@ func (s myProfileServer) UpdateProfile(ctx context.Context, in *protos.ProfileRe
 	return new(protos.Empty), nil
 }
 
+// isValidEmailFormat checks if the given email is in a valid format.
+func isValidEmailFormat(email string) bool {
+	email = strings.TrimSpace(email) // Trim leading and trailing whitespaces
+	// Perform a simple check for '@' and '.com'
+	return strings.Contains(email, "@") && strings.HasSuffix(email, ".com")
+}
+
 func (s myProfileServer) DeleteProfile(ctx context.Context, in *protos.ProfileRequest) (*protos.Empty, error) {
-	err := s.repo.DeleteByEmail(in.GetEmail())
+	err := s.repo.DeleteByEmail(strings.TrimSpace(in.GetEmail())) // Added trim here
 	if err != nil {
 		return nil, err
 	}
 	return new(protos.Empty), nil
-}
-
-// isValidEmailFormat checks if the given email is in a valid format.
-func isValidEmailFormat(email string) bool {
-	// Perform a simple check for '@' and '.com'
-	return strings.Contains(email, "@") && strings.HasSuffix(email, ".com")
 }
